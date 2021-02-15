d631a007eef6894585e021553e538fb0c01d6cd8
microchelick43018 <dyatlovkonstan@gmail.com>
2021-02-16 02:24:53 +0300

    123

--- Program.cs
+++ Program.cs
@@ -60,7 +60,7 @@ namespace AdvancedDynamicArray
                 }
                 else if (command == "exit")
                 {
-                    continue;
+                    return;
                 }
                 else
                 {
